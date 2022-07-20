88056ed2616d7ab92c45d8588d78145ef325aee3
Dev <saad.jamil@devsinc.com>
2022-07-20 14:56:15 +0500

    added dfr-yasg

--- API/urls.py
+++ API/urls.py
@@ -14,17 +14,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from rest_framework_swagger.views import get_swagger_view
+from django.urls import path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from main.views import *
 from rest_framework_simplejwt import views as jwt_views
 
-schema_view = get_swagger_view(title="Object API")
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', schema_view),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/object/<int:pk>', handle_object, name='retrieve-object'),
     path('api/object/', create_list_object),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
